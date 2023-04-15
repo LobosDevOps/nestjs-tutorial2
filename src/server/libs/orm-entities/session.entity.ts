190427b77f1dcd935e013a6a41fd32c2234121f8
ryoichi <tachibana.ryoichi@lobos-works.com>
2023-04-16 00:59:39 +0900

    add entity

---
+++ src/server/libs/orm-entities/session.entity.ts
@@ -0,0 +1,24 @@
+import { ISession } from 'connect-typeorm';
+import {
+  Column,
+  Entity,
+  Index,
+  PrimaryColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+@Entity('session')
+export class SessionEntity implements ISession {
+  @Index()
+  @Column('bigint')
+  expiredAt = Date.now();
+
+  @PrimaryColumn('varchar', { length: 255 })
+  id = '';
+
+  @Column('text')
+  json = '';
+
+  @DeleteDateColumn()
+  destroyedAt?: Date;
+}
