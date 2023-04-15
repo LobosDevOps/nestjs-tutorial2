1b5687d1a81487d0595e9f4ea04dfab0d01c55ca
ryoichi <tachibana.ryoichi@lobos-works.com>
2023-04-16 01:00:03 +0900

    add module

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,7 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+
+import * as Entities from './libs/orm-entities';
+import { CustomersModule } from './modules/customers/customers.module';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    CustomersModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '0000',
+      database: 'tutorial_db2',
+      entities: Object.values(Entities),
+      synchronize: true,
+    }),
+    AuthModule,
+    PassportModule.register({
+      session: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
